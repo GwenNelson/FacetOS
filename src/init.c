44b73fef7843163625b7f40eebff7df9bba63ba4
Gwen Nelson <gwen@gwennelson.co.uk>
2026-08-16 23:14:13 +0100

    the fucker is almost ready to boot

--- src/init.c
+++ src/init.c
@@ -1,5 +1,15 @@
 #include <sel4/sel4.h>
 
+// temporary - we'll build a klibc or something later
+char *strcpy(char *dest, const char *src) {
+    char *ret = dest;
+
+    while ((*dest++ = *src++) != '\0')
+        ;
+
+    return ret;
+}
+
 int main() {
     seL4_DebugPutString("FacetOS says hello!\n");
 
