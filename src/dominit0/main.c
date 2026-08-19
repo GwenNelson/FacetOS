b0559fa05218c8f0460cf92ec8fd19e3def8ecde
Gwen Nelson <gwen@gwennelson.co.uk>
2026-08-19 01:21:42 +0100

    dumb kmalloc test

--- src/dominit0/main.c
+++ src/dominit0/main.c
@@ -1,10 +1,17 @@
 #include <facetos/dominit0/klog.h>
+#include <facetos/dominit0/kmalloc.h>
 #include <sel4/sel4.h>
 
 void main(int argc, char **argv, char **envp) {
      klog_init_early();
      klog(LOG_INFO,"Starting FacetOS...\n");
      klog_dump_debug();
+     kmalloc_init_early();
+     klog(LOG_DEBUG,"Testing an allocation...\n");
+     void* my_buf = kmalloc(32);
+     klog(LOG_DEBUG,"Allocated 32 bytes\n");
+     kfree(my_buf);
+     klog(LOG_DEBUG,"If we're still here, yay, it didn't crash\n");
      for (;;) {
         seL4_Yield();
      }
