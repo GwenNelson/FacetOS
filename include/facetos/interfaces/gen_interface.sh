030c93e36b6c8ac21bd2fd224ab6378c92cbcc60
Gwen Nelson <gwen@gwennelson.co.uk>
2026-08-19 22:14:09 +0100

    fix gen_interface.sh

--- include/facetos/interfaces/gen_interface.sh
+++ include/facetos/interfaces/gen_interface.sh
@@ -14,11 +14,23 @@ cat << EOF
 #pragma once
 
 #include <facetos/uuid.h>
+#include <facetos/interfaces/IGenericObject.h>
+
+#include <stddef.h>
+
 static const uuid_t IID_${interface_name} = UUID_INIT(0x${uuid_no_dash:0:8},0x${uuid_no_dash:8:4},0x${uuid_no_dash:12:4},0x${uuid_no_dash:16:4},0x${uuid_no_dash:20:12}ULL);
 
+static const size_t uuid_t ${interface_name}_RequiredInterfaces[] = {
+	IID_IGenericObject, // this is needed by everything
+};
+
+static const  ${interface_name}_RequiredInterfacesCount = sizeof(${interface_name}_RequiredInterfaces) / sizeof(${interface_name}_RequiredInterfaces[0]);
+
 typedef struct ${interface_name} {
-	void *self; // required by ALL interfaces
-	void *priv; // private data
+	void   *self; // required by ALL interfaces
+	void   *priv; // private data
+	void* (*getInterface)(void* self, uuid_t iid);  // Returns the requested interface exposed by this object,
+							// or NULL if the object does not support it.
 	// fill in your methods and variables here, remember to always make first argument void* self
 } ${interface_name};
 EOF
